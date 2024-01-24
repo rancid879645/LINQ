08130adfc5944fc735f4c9e4ad60ff548bef1437
Jonathan Velosa <jonathan_velosa@epam.com>
2024-01-24 09:33:14 -0500

    Revert "All point of tas was implemented:"
    This reverts commit 9e97ae59b11028b81e187cc567b5a32cfc5d938a. jv:

--- Task1.Tests/LinqTaskTests.cs
+++ Task1.Tests/LinqTaskTests.cs
@@ -13,46 +13,21 @@ namespace Task1.Tests
         [TestCase(0, ExpectedResult = 6)]
         [TestCase(-1, ExpectedResult = 10)]
         [TestCase(1, ExpectedResult = 5)]
-        public int GetCustomersByTotalOrderSum_Should_ReturnsCustomersCount_Acording_With_Limit(decimal limit)
+        public int Linq1_Limit_ReturnsCustomersCount(decimal limit)
         {
-            return LinqTask.GetCustomersByTotalOrderSum(DataSource.Customers, limit).Count();
+            return LinqTask.Linq1(DataSource.Customers, limit).Count();
         }
 
         [Test]
-        public void GetCustomersByTotalOrderSum_NullSource_Should_ThrowsArgumentNullException()
+        public void Linq1_NullSource_ThrowsArgumentNullException()
         {
-            Assert.That(() => LinqTask.GetCustomersByTotalOrderSum(null, 42).ToList(), Throws.ArgumentNullException);
+            Assert.That(() => LinqTask.Linq1(null, 42).ToList(), Throws.ArgumentNullException);
         }
 
         [Test]
-        public void GetTheFirstOrderDateFromAllCustomers_NullSource_Should_ThrowsArgumentNullException()
+        public void Linq2_CustomersAndSuppliers_2CustomersHaveSuppliers()
         {
-            Assert.That(() => LinqTask.GetTheFirstOrderDateFromAllCustomers(null).ToList(), Throws.ArgumentNullException);
-        }
-
-        [Test]
-        public void GetTheFirstOrderDateFromAllCustomers_Should_Return_AllCustomers_With_The_First_Order_Date()
-        {
-            var result = LinqTask.GetTheFirstOrderDateFromAllCustomers(DataSource.Customers).ToList();
-            Assert.That(() => result.Count, Is.EqualTo(6));
-            foreach (var (customer, orderDate) in result)
-            {
-                Assert.That(orderDate, Is.Not.Null);
-            }
-        }
-
-        [Test]
-        public void GetTheFirstOrderDateFromAllCustomersOrdered_Should_Return_AllCustomers_With_The_First_Order_Date()
-        {
-            var result = LinqTask.GetTheFirstOrderDateFromAllCustomersOrdered(DataSource.Customers).ToList();
-            Assert.That(() => result.Count, Is.EqualTo(6));
-            Assert.That(result[0].orderDate.ToString("dd/MM/yyyy"), Is.EqualTo(FindCustomerOrdersMinDate(result[0].customer).ToString("dd/MM/yyyy")));
-        }
-
-        [Test]
-        public void GetAllCustomersWithSupplierByCity_Should_Return_CustomersAndSuppliers()
-        {
-            var result = LinqTask.GetAllCustomersWithSupplierByCity(DataSource.Customers, DataSource.Suppliers).ToList();
+            var result = LinqTask.Linq2(DataSource.Customers, DataSource.Suppliers).ToList();
 
             Assert.That(() => result.Count, Is.EqualTo(DataSource.Customers.Count));
             foreach (var (customer, suppliers) in result)
@@ -66,38 +41,231 @@ namespace Task1.Tests
         }
 
         [Test]
-        public void GetAllCustomersWithSupplierByCity_NullCustomer_Should_ThrowsArgumentNullException()
+        public void Linq2_NullCustomer_ThrowsArgumentNullException()
         {
-            Assert.That(() => LinqTask.GetAllCustomersWithSupplierByCity(null, null).ToList(), Throws.ArgumentNullException);
+            Assert.That(() => LinqTask.Linq2(null, null).ToList(), Throws.ArgumentNullException);
         }
 
         [Test]
-        public void GetCustomersWithIssueInLocationParameters_Should_Return_AllCustomers_With_Incorrect_Data_In_Location_Parameters()
+        public void Linq2UsingGroup_CustomersAndSuppliers_2CustomersHaveSuppliers()
         {
-            var result = LinqTask.GetCustomersWithIssueInLocationParameters(DataSource.Customers).ToList();
-            Assert.That(() => result.Count, Is.EqualTo(5));
-            Assert.That(result[0].Phone, Is.EqualTo("555-7788"));
+            var result = LinqTask.Linq2UsingGroup(DataSource.Customers, DataSource.Suppliers).ToList();
+
+            Assert.That(() => result.Count, Is.EqualTo(DataSource.Customers.Count));
+            foreach (var (customer, suppliers) in result)
+            {
+                foreach (var supplier in suppliers)
+                {
+                    StringAssert.AreEqualIgnoringCase(customer.City, supplier.City);
+                    StringAssert.AreEqualIgnoringCase(customer.Country, supplier.Country);
+                }
+            }
         }
 
         [Test]
-        public void GetProductsGroupedByCategory_Should_Return_AllCustomers_With_Incorrect_Data_In_Location_Parameters()
+        public void Linq2UsingGroup_NullCustomer_ThrowsArgumentNullException()
         {
-            var result = LinqTask.GetProductsGroupedByCategory(DataSource.Products).ToList();
-            Assert.That(() => result.Count, Is.EqualTo(2));
+            Assert.That(() => LinqTask.Linq2UsingGroup(null, null).ToList(), Throws.ArgumentNullException);
+        }
+
+        [TestCase(800, ExpectedResult = 2)]
+        [TestCase(0, ExpectedResult = 6)]
+        [TestCase(-1, ExpectedResult = 6)]
+        [TestCase(1, ExpectedResult = 5)]
+        public int Linq3_Limit_ReturnsCustomersCount(decimal limit)
+        {
+            return LinqTask.Linq3(DataSource.Customers, limit).Count();
         }
 
         [Test]
-        public void GetAverageProfitabilityByCity_Should_Return_All_Cities_With_product_Average()
+        public void Linq3_NullCustomer_ThrowsArgumentNullException()
         {
-            var result = LinqTask.GetAverageProfitabilityByCity(DataSource.Customers).ToList();
-            Assert.That(() => result.Count, Is.EqualTo(6));
+            Assert.That(() => LinqTask.Linq3(null, 42).ToList(), Throws.ArgumentNullException);
         }
 
         [Test]
-        public void GetAllCitiesOrderdByName_Should_Return_All_Cities_In_a_Single_Line_Ordered()
+        public void Linq4_Customers_CustomersAndDateOfEntry()
         {
-            var result = LinqTask.GetAllCitiesOrderedByName(DataSource.Customers);
-            Assert.That(result, Is.EqualTo("-USA-Berlin-London-Warszawa-Sao Paulo-Mexico D.F."));
+            var result = LinqTask.Linq4(DataSource.Customers).ToList();
+
+            Assert.That(() => result.Count, Is.EqualTo(DataSource.Customers.Count - 4));
+            foreach (var (customer, dateOfEntry) in result)
+            {
+                Assert.That(FindCustomerOrdersMinDate(customer), Is.EqualTo(dateOfEntry));
+            }
+        }
+
+        [Test]
+        public void Linq4_NullCustomer_ThrowsArgumentNullException()
+        {
+            Assert.That(() => LinqTask.Linq4(null).ToList(), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Linq5_Customers_CustomersAndDateOfEntry()
+        {
+            var result = LinqTask.Linq5(DataSource.Customers).ToList();
+
+            Assert.That(() => result.Count, Is.EqualTo(DataSource.Customers.Count - 4));
+            foreach (var (customer, dateOfEntry) in result)
+            {
+                Assert.That(FindCustomerOrdersMinDate(customer), Is.EqualTo(dateOfEntry));
+            }
+
+            Assert.That(result[0].customer, Is.EqualTo(DataSource.Customers[6]));
+            Assert.That(result[1].customer, Is.EqualTo(DataSource.Customers[1]));
+            Assert.That(result[2].customer, Is.EqualTo(DataSource.Customers[3]));
+            Assert.That(result[3].customer, Is.EqualTo(DataSource.Customers[2]));
+            Assert.That(result[4].customer, Is.EqualTo(DataSource.Customers[4]));
+            Assert.That(result[5].customer, Is.EqualTo(DataSource.Customers[0]));
+        }
+
+        [Test]
+        public void Linq5_NullCustomer_ThrowsArgumentNullException()
+        {
+            Assert.That(() => LinqTask.Linq5(null).ToList(), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Linq6_Customers_ReturnsFilteredCustomers()
+        {
+            var expectedResult = DataSource.Customers.ToList();
+            expectedResult.RemoveAt(0);
+            expectedResult.RemoveAt(0);
+            expectedResult.RemoveAt(3);
+
+            var result = LinqTask.Linq6(DataSource.Customers).ToList();
+
+            CollectionAssert.AreEquivalent(expectedResult, result);
+        }
+
+        [Test]
+        public void Linq6_NullCustomer_ThrowsArgumentNullException()
+        {
+            Assert.That(() => LinqTask.Linq6(null).ToList(), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Linq7_Customers_Returns5()
+        {
+            var expectedResult = new[]
+            {
+                new Linq7CategoryGroup
+                {
+                    Category = "Beverages",
+                    UnitsInStockGroup = new[]
+                    {
+                        new Linq7UnitsInStockGroup
+                        {
+                            UnitsInStock = 39,
+                            Prices = new [] { 19.0000M }
+                        },
+                        new Linq7UnitsInStockGroup
+                        {
+                            UnitsInStock = 17,
+                            Prices = new [] { 18.0000M }
+                        }
+                    }
+                },
+                new Linq7CategoryGroup
+                {
+                    Category = "Condiments",
+                    UnitsInStockGroup = new[]
+                    {
+                        new Linq7UnitsInStockGroup
+                        {
+                            UnitsInStock = 15,
+                            Prices = new [] { 10.0000M, 40.0000M }
+                        },
+                        new Linq7UnitsInStockGroup
+                        {
+                            UnitsInStock = 13,
+                            Prices = new [] { 30.0000M }
+                        }
+                    }
+                }
+            };
+
+            var result = LinqTask.Linq7(DataSource.Products);
+
+            foreach (var categoryGroup in result)
+            {
+                var expectedCategoryGroup = expectedResult.Single(_ => _.Category == categoryGroup.Category);
+                foreach (var unitInStockGroup in categoryGroup.UnitsInStockGroup)
+                {
+                    var expectedUnitInStockGroup = expectedCategoryGroup
+                        .UnitsInStockGroup.Single(_ => _.UnitsInStock == unitInStockGroup.UnitsInStock);
+                    CollectionAssert.AreEqual(expectedUnitInStockGroup.Prices, unitInStockGroup.Prices);
+                }
+            }
+        }
+
+        [Test]
+        public void Linq7_NullProducts_ThrowsArgumentNullException()
+        {
+            Assert.That(() => LinqTask.Linq7(null).ToList(), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Linq8_Products_ReturnsGroupedProducts()
+        {
+            decimal cheap = 10, middle = 30, expensive = 40;
+            var result = LinqTask.Linq8(DataSource.Products, cheap, middle, expensive).ToList();
+
+            var cheapProducts = result.Single(_ => _.category == cheap).products;
+            Assert.That(cheapProducts.Count(), Is.EqualTo(1));
+            var middleProducts = result.Single(_ => _.category == middle).products;
+            Assert.That(middleProducts.Count(), Is.EqualTo(3));
+            var expensiveProducts = result.Single(_ => _.category == expensive).products;
+            Assert.That(expensiveProducts.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Linq8_NullProducts_ThrowsArgumentNullException()
+        {
+            Assert.That(() => LinqTask.Linq8(null, 42, 42, 42).ToList(), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Linq9_Customers_ReturnsGroupedProducts()
+        {
+            var expected = new List<(string city, int averageIncome, int averageIntensity)>
+            {
+                ("Berlin", 2023, 3),
+                ("Mexico D.F.", 680, 2),
+                ("London", 690, 1),
+                ("Warszawa", 1, 0),
+                ("Sao Paulo", 0, 0),
+                ("USA", 0, 0)
+            };
+
+            var result = LinqTask.Linq9(DataSource.Customers).ToList();
+
+            foreach (var valueTuple in result)
+            {
+                var expectedValue = expected.Single(_ => _.city == valueTuple.city);
+                Assert.That(expectedValue.averageIncome, Is.EqualTo(valueTuple.averageIncome));
+                Assert.That(expectedValue.averageIntensity, Is.EqualTo(valueTuple.averageIntensity));
+            }
+        }
+
+        [Test]
+        public void Linq9_NullCustomers_ThrowsArgumentNullException()
+        {
+            Assert.That(() => LinqTask.Linq9(null).ToList(), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Linq10_Suppliers_ReturnsAggregateString()
+        {
+            string result = LinqTask.Linq10(DataSource.Suppliers);
+            StringAssert.AreEqualIgnoringCase("UKUSAJapanSpainBrazilSwedenGermanyAustralia", result);
+        }
+
+        [Test]
+        public void Linq10_NullSuppliers_ThrowsArgumentNullException()
+        {
+            Assert.That(() => LinqTask.Linq10(null).ToList(), Throws.ArgumentNullException);
         }
 
         private static DateTime FindCustomerOrdersMinDate(Customer customer)
--- Task1/LinqTask.cs
+++ Task1/LinqTask.cs
@@ -1,145 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using Microsoft.VisualBasic;
 using Task1.DoNotChange;
 
 namespace Task1
 {
     public static class LinqTask
     {
-        public static IEnumerable<Customer> GetCustomersByTotalOrderSum(IEnumerable<Customer> customers, decimal limit)
+        public static IEnumerable<Customer> Linq1(IEnumerable<Customer> customers, decimal limit)
         {
-            if (customers == null)
-                throw new ArgumentNullException();
-
-            var result = from customer in customers
-                         where customer.Orders.Sum(order => order.Total) > limit
-                         select customer;
-
-            return result;
+            throw new NotImplementedException();
         }
 
-        public static IEnumerable<(Customer customer, DateTime orderDate)> GetTheFirstOrderDateFromAllCustomers(IEnumerable<Customer> customers)
-        {
-            if (customers == null)
-                throw new ArgumentNullException();
-
-            var result = from customer in customers
-                         where customer.Orders.Any()
-                         let firstOrderDate = customer.Orders.Min(order => order.OrderDate)
-                         select (customer, firstOrderDate);
-            return result;
-        }
-
-        public static IEnumerable<(Customer customer, DateTime orderDate)> GetTheFirstOrderDateFromAllCustomersOrdered(IEnumerable<Customer> customers)
-        {
-            if (customers == null)
-                throw new ArgumentNullException();
-
-            var result = from customer in customers
-                         where customer.Orders.Any()
-                         let firstOrderDate = customer.Orders.Min(order => order.OrderDate)
-                         orderby firstOrderDate.Year,
-                             firstOrderDate.Month,
-                             customer.Orders.Sum(order => order.Total) descending,
-                             customer.CompanyName
-                         select
-                             (
-                             customer,
-                             firstOrderDate
-                             );
-            return result;
-        }
-
-        public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> GetAllCustomersWithSupplierByCity(
+        public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2(
             IEnumerable<Customer> customers,
             IEnumerable<Supplier> suppliers
         )
         {
-            if (customers == null)
-                throw new ArgumentNullException();
-
-            var result = from customer in customers
-                         join supplier in suppliers on customer.City equals supplier.City into customerSuppliers
-                         select (customer, customerSuppliers);
-
-            return result;
+            throw new NotImplementedException();
         }
 
-        public static IEnumerable<Customer> GetCustomersWithIssueInLocationParameters(
-            IEnumerable<Customer> customers)
+        public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2UsingGroup(
+            IEnumerable<Customer> customers,
+            IEnumerable<Supplier> suppliers
+        )
         {
-            if (customers == null)
-                throw new ArgumentNullException();
-
-            var result = from customer in customers
-                         where !IsAValidPostalCode(customer.PostalCode)
-                               || customer.Region == string.Empty
-                               || !ContainsParenthesis(customer.Phone)
-                         select customer;
-
-            return result;
+            throw new NotImplementedException();
         }
 
-        private static bool IsAValidPostalCode(string postalCode)
+        public static IEnumerable<Customer> Linq3(IEnumerable<Customer> customers, decimal limit)
         {
-            var result = postalCode.All(char.IsDigit);
-            return result;
+            throw new NotImplementedException();
         }
 
-        private static bool ContainsParenthesis(string phoneNumber)
+        public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq4(
+            IEnumerable<Customer> customers
+        )
         {
-            return phoneNumber.Contains("(") && phoneNumber.Contains(")");
+            throw new NotImplementedException();
         }
 
-        public static IEnumerable<object> GetProductsGroupedByCategory(
-            IEnumerable<Product> products)
+        public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq5(
+            IEnumerable<Customer> customers
+        )
         {
-            if (products == null)
-                throw new ArgumentNullException();
-
-            var result = from product in products
-                         group product by new { product.Category, availble = product.UnitsInStock > 0 } into grouped
-                         orderby grouped.Min(p => p.UnitPrice)
-                         select new
-                         {
-                             Category = grouped.Key.Category,
-                             InStock = grouped.Key.availble
-                         };
-
-            return result;
+            throw new NotImplementedException();
         }
 
-        public static IEnumerable<object> GetAverageProfitabilityByCity(IEnumerable<Customer> customers)
+        public static IEnumerable<Customer> Linq6(IEnumerable<Customer> customers)
         {
-            if (customers == null)
-                throw new ArgumentNullException();
-
-            var result = from customer in customers
-                         group customer by customer.City into groupedByCity
-                         select new
-                         {
-                             City = groupedByCity.Key,
-                             AverageProfitability = groupedByCity.Average(c => c.Orders.Sum(o => o.Total)),
-                             AverageRate = groupedByCity.Average(c => c.Orders.Count())
-                         };
-
-            return result;
+            throw new NotImplementedException();
         }
 
-        public static string GetAllCitiesOrderedByName(IEnumerable<Customer> customers)
+        public static IEnumerable<Linq7CategoryGroup> Linq7(IEnumerable<Product> products)
         {
-            if (customers == null)
-                throw new ArgumentNullException();
+            /* example of Linq7result
 
-            var result = from customer in customers
-                         group customer by new { customer.City, customer.Country } into groupedByCity
-                         orderby groupedByCity.Key.City.Length, groupedByCity.Key.Country
-                         select groupedByCity.Key.City;
+             category - Beverages
+	            UnitsInStock - 39
+		            price - 18.0000
+		            price - 19.0000
+	            UnitsInStock - 17
+		            price - 18.0000
+		            price - 19.0000
+             */
 
-            return result.Aggregate(string.Empty, (current, city) => current + $"-{city}");
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(decimal category, IEnumerable<Product> products)> Linq8(
+            IEnumerable<Product> products,
+            decimal cheap,
+            decimal middle,
+            decimal expensive
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<(string city, int averageIncome, int averageIntensity)> Linq9(
+            IEnumerable<Customer> customers
+        )
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Linq10(IEnumerable<Supplier> suppliers)
+        {
+            throw new NotImplementedException();
         }
     }
 }
